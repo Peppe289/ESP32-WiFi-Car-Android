5e3413f2ef7bc4baa733466e998da00458edc33e
Peppe289 <gsperanza204@protonmail.com>
2025-08-28 11:13:34 +0200

    upload initial app source

---
+++ app/src/main/java/com/peppe289/esp32wificar/MainActivity.java
@@ -0,0 +1,130 @@
+package com.peppe289.esp32wificar;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView tv = findViewById(R.id.pingText);
+        Button left = findViewById(R.id.button);
+        Button right = findViewById(R.id.button2);
+        WebSocketESP32 webSocketESP32 = new WebSocketESP32();
+        try {
+            webSocketESP32.connectionRunner();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        webSocketESP32.callback = new WebSocketESP32.OnPingCallBack() {
+            @Override
+            public void update(long ms) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(String.format("%d ms", ms));
+                    }
+                });
+            }
+        };
+
+        left.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Quando premi il pulsante
+                        Log.d("Button", "Premuto");
+                        webSocketESP32.changeSteering(1);
+                        return true; // consuma l'evento
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        // Quando rilasci il pulsante
+                        Log.d("Button", "Rilasciato");
+                        webSocketESP32.changeSteering(0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        right.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Quando premi il pulsante
+                        Log.d("Button", "Premuto");
+                        webSocketESP32.changeSteering(-1);
+                        return true; // consuma l'evento
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        // Quando rilasci il pulsante
+                        Log.d("Button", "Rilasciato");
+                        webSocketESP32.changeSteering(0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(200);
+        seekBar.setProgress(100);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int direction = Integer.compare(i, 100);
+                if (direction == 0)
+                    webSocketESP32.changeForword(0, 0);
+                else {
+                    int value = i - 100;
+                    if (value < 0) value = -value;
+                    int basePower = 160;
+                    int maxPower = 255;
+                    int normalized = (int) Math.round(basePower + (maxPower - basePower) * Math.pow(value / 100.0, 2.0));
+                    webSocketESP32.changeForword(-direction, normalized);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                seekBar.setProgress(seekBar.getMax() / 2);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/peppe289/esp32wificar/WebSocketESP32.java
@@ -0,0 +1,136 @@
+package com.peppe289.esp32wificar;
+
+import android.util.Log;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Random;
+
+public class WebSocketESP32 {
+    private int steering = 0;
+    private int[] forword;
+    private String URL = "ws://192.168.4.1:81";
+
+    public OnPingCallBack callback;
+
+    HashMap<Integer, Long> timeRegister;
+
+    WebSocketESP32() {
+        timeRegister = new HashMap<>();
+    }
+
+    public void changeSteering(int sg) {
+        if (sg != steering && (sg == -1 || sg == 0 || sg == 1)) {
+            steering = sg;
+            //Log.d("STEERING", "Nuovo valore: " + steering);
+        }
+    }
+
+    public void changeForword(int direction, int speed) {
+
+        if (forword[0] != direction && (direction == -1 || direction == 1 || direction == 0)) {
+            forword[0] = direction;
+            //Log.d("FORWORD", "Nuovo valore: " + forword[0]);
+        }
+
+        if (speed == forword[1]) return;
+
+        forword[1] = Math.min(speed, 255);
+    }
+
+    public void connectionRunner() throws URISyntaxException {
+        URI uri = new URI(URL);
+        forword = new int[2];
+        forword[0] = 0;
+        forword[1] = 0;
+
+
+        WebSocketClient client = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake handshake) {
+                System.out.println("Connessione aperta!");
+            }
+
+            @Override
+            public void onMessage(String message) {
+                //System.out.println("Messaggio ricevuto: " + message);
+                long now = System.currentTimeMillis();
+
+                try {
+                    int key = Integer.parseInt(message);
+                    long reqTime = timeRegister.get(key);
+                    //Log.d("PING", "Request Ping: " + (now - reqTime) + " ms");
+                    timeRegister.remove(key);
+                    callback.update(now - reqTime);
+                } catch (NullPointerException ignored) {}
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                Log.d("WebSocket", "Chiusa: code=" + code + " reason=" + reason + " remote=" + remote);
+                callback.update(-1);
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+
+        new Thread(() -> {
+            try {
+                client.connectBlocking();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                boolean canSendSteering = true;
+                long lastRequestDate = 0;
+
+                while (true) {
+                    if (client.isOpen()) {
+
+                        long timeNow = System.currentTimeMillis();
+                        if (lastRequestDate == 0 || (timeNow - lastRequestDate) > 500) {
+                            lastRequestDate = timeNow;
+                            Random rd = new Random();
+                            int code = rd.nextInt(255);
+                            timeRegister.put(code, timeNow);
+                            client.send("P," + code);
+                        }
+
+                        // send steering signal only if first 0 or if isn't 0
+                        if (steering != 0 || canSendSteering) {
+                            String motorA = "A," + ((steering == 0) ? "0," : "240,") + steering;
+                            client.send(motorA);
+                            canSendSteering = true;
+                        }
+
+                        if (steering == 0)
+                            canSendSteering = false;
+
+                        String motorB = "B," + forword[1] + "," + forword[0];
+                        client.send(motorB);
+
+                        //Log.d("SEND", motorA);
+                        //Log.d("SEND", motorB);
+                    }
+                    Thread.sleep(50);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public interface OnPingCallBack {
+        public void update(long ms);
+    }
+}
